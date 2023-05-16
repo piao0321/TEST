1358ceee7de6b65dd70cf16c8c5d2e91f03da4ce
Pupil <piao0321>
2023-05-16 17:32:40 +0800

    Initial commit

---
+++ test1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Time :2023/5/16 17:26
+@Author :Lai Xiangyuan
+@Email :2936885192@qq.com
+@File :test1.py
+@ID :12003990122
+"""
+
---
+++ test2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@Time :2023/5/16 17:26
+@Author :Lai Xiangyuan
+@Email :2936885192@qq.com
+@File :test2.py
+@ID :12003990122
+"""
